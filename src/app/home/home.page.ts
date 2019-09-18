12e1d63e4b65ce102df0405daa3718d058652be0
Cleilson de Sousa Pereira <cleilsonpereira@fgf.ce>
2019-09-18 11:36:04 -0300

    mapElement for show GoogleMaps include

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Platform, LoadingController } from '@ionic/angular';
+import { Environment } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild('map') mapElement: any;
+  private loading: any;
 
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private loadingCtrl: LoadingController
+    ) {}
+
+  ngOnInit() {
+    this.mapElement = this.mapElement.nativeElement;
+
+    this.mapElement.style.width = this.platform.width() + 'px';
+    this.mapElement.style.height = this.platform.height() + 'px';
+
+    this.loadMap();
+
+  }
+
+  async loadMap() {
+    this.loading = await this.loadingCtrl.create({ message: 'Por favor aguarde...' });
+    await this.loading.present();
+
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    })
+  }
 
 }
